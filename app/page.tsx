aab265d73e5ec1b34a352eee57b1f6b666dc78b7
Marius Dulce <m.dulce@nectlc.com>
2024-09-03 12:43:09 +0200

    tutto ok

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,17 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Auguri aurora, simone 🔫🔫 
+        Welcome
       </h1>
-     
+      <p className="mb-4">
+        {`This is my blogging website. I'm a web developer and I love to write about my experiences and learnings,`} {`you can find all`} <a className="underline" href="/blog">{`my posts here.`}</a>
+      </p>
+      <p className="mb-4">
+        {`If you have any questions or suggestions, feel free to reach out to me via`} <a className="underline" href="mailto:dulcemariuspriv@gmail.com">{`e-mail`}</a>
+      </p>
+      <p className="mb-4">
+        {`Have a great day!`}
+      </p>
     </section>
   )
 }
